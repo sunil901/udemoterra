3281f2c4b471f742301f14c17ddccb6472d7aebf
ep2_proj <94438718+sunil901@users.noreply.github.com>
2024-10-26 23:44:14 +0530

    Update backend.tf

--- backend.tf
+++ backend.tf
@@ -2,6 +2,6 @@ terraform {
   backend "s3" {
     bucket = "udemo-s3b1" # Replace with your actual S3 bucket name
     key    = "EKS/terraform.tfstate"
-    region = "ap-south-1"
+    region = "ap-northeast-3"
   }
 }
