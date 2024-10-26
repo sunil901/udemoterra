57e933927ddb1b3acc89414c48673b3ce6187214
ep2_proj <94438718+sunil901@users.noreply.github.com>
2024-10-27 00:14:58 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -14,7 +14,7 @@ data "aws_iam_policy_document" "assume_role" {
 
 # Create IAM Role for EKS Cluster
 resource "aws_iam_role" "example" {
-  name               = "eks-cluster-cloud"
+  name               = "eks-cluster-cloud-009"
   assume_role_policy = data.aws_iam_policy_document.assume_role.json
 }
 
